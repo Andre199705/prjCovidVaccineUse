82cbea668f35e1d6d44e902579c70e5e18188e48
André Reynecke <20102400@vcconnect.co.za>
2021-03-17 16:53:09 -0700

    Add project files.

---
+++ prjCovidVaccineUse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prjCovidVaccineUse
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            info A = new info();
+            Console.WriteLine("please enter wich hospital");
+            A.setHospital_Name(Console.ReadLine());
+
+            Console.WriteLine("please enter the address");
+            A.setAddress(Console.ReadLine());
+
+            Console.WriteLine("please indicate if it as a private or an public Hospital ");
+            A.setHospital_Name(Console.ReadLine());
+            //capture the info from the capture class
+            Capture c = new Capture();
+            c.addPerson();
+
+            //Print all information that is inside the class 
+            printDetails p = new printDetails();
+            p.PrintItems();
+
+
+        }
+    }
+}
---
+++ prjCovidVaccineUse/printDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prjCovidVaccineUse
+{
+    class printDetails
+    {
+        public void PrintItems()
+        {
+
+
+
+            info A = new info();
+
+            Console.Clear();
+
+            Console.WriteLine("Covid 19 VACCINE");
+            Console.WriteLine("Address" + A.Address);
+            Console.WriteLine("" + A.getHospital_Name());
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
